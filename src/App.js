28593fc9ae4eb9bbf99ecfab157e7757ec6c7c78
Dan Kiss <flowfast47@gmail.com>
2020-09-10 11:45:21 -0700

    modify use effect hook

--- src/App.js
+++ src/App.js
@@ -5,25 +5,44 @@ import './App.css';
 
 function App() {
   const [results, setResults] = useState([])
+  const [query, setQuery] = useState('react hooks');
+  console.log(9, results, query)
+
   useEffect(() => {
-    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-      .then(response => {
-        console.log(10, response.data)
-        setResults(response.data.hits)
-        console.log(13, results)
-      })
-  }, []);
+    getResults()
+    // .then(response => {
+    //   console.log(10, response.data)
+    //   setResults(response.data.hits)
+    //   console.log(13, results)
+    // })
+  },[]);
+
+  const getResults = async () => {
+    const response = await axios.get(
+      `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+    setResults(response.data.hits)
+  }
+
+  const handleSearch = event => {
+    event.preventDefault()
+    getResults()
+  }
 
   console.log(17, results)
   return (
     <div>
-      <ul>
-        {results.map((result, index) => (
-          <li key={result.ObjectId}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleSearch}>
+        <input type="text" onChange={event => setQuery(event.target.value)} value={query} />
+        <button type="submit">Submit</button>
+        <ul>
+          {results.map((result, index) => (
+            <li key={result.ObjectId}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
